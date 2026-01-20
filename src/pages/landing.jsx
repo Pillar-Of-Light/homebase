9656aee8735d9d3dce7e28cc1492d49dfa02d8e9
Yilamu Lafeier <143864267+YilamuLafeier@users.noreply.github.com>
2026-01-19 20:10:53 -1000

    create separate pages and set up landing page

---
+++ src/pages/landing.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Header from "../components/layout/header";
+import Footer from "../components/layout/footer";
+import Landing from "../components/sections/landing";
+import Doctrine from "../components/sections/doctrine";
+import Music from "../components/sections/music";
+import Visuals from "../components/sections/visuals";
+import Writings from "../components/sections/writings";
+import Communion from "../components/sections/communion";
+
+export default function HomePage() {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <Landing />
+        <Doctrine />
+        <Music />
+        <Visuals />
+        <Writings />
+        <Communion />
+      </main>
+      <Footer />
+    </div>
+  );
+}
